157c1ea92d1335088dc55cde7ac7b6b9f555bbd1
olena-olena <vykaliuk@gmail.com>
2019-11-11 20:02:18 +0200

    added all the tests

--- stringToSMS.js
+++ stringToSMS.js
@@ -2,8 +2,8 @@ function solution (S, K) {
 
 	// test if input is valid for function
 	if (typeof(K)!=='number' || Math.floor(K)!==K || K<1 || K>500 ||
-		typeof(S)!=='string' || S==='' || S.length>500 || S.includes('  ') ||
-		S[0]===' ' ||S[S.length-1]===' ' || /[^a-z ]/i.test(S) ) {
+		typeof(S)!=='string' || /[^a-z ]/i.test(S) || S.includes('  ') ||
+		S==='' || S.length>500  || S[0]===' ' || S[S.length-1]===' ') {
 		console.log('error: invalid input');
 		return;
 	}
@@ -42,20 +42,93 @@ function solution (S, K) {
 
 
 
-// test area
+// test function
 
-// ok
+// S > K, anything in S < K
 let S = 'SMS messases are really short';
 let K = 12;
-console.log(solution (S, K), '\n'); // output: 3
+console.log(solution (S, K)); // output: 3
 
-// K is too small
-S = 'SMS messases are really short';
-K = 3;
-console.log(solution (S, K), '\n'); // output: -1
+// S > K, something in S > K
+S = 'hello iAmEight';
+K = 7;
+console.log(solution (S, K)); // output: -1
 
-// wrong characters in string
-S = '#@)₴?$0';
-K = 10;
-console.log(solution (S, K), '\n'); // output: error: invalid input; undefined 
+// S < K
+S = 'iAmEight';
+K = 9;
+console.log(solution (S, K)); // output: 1
 
+// S = K
+S = 'iAmEight';
+K = 8;
+console.log(solution (S, K)); // output: 1
+
+// last item in S = K
+S = 'hello iAmEight';
+K = 8;
+console.log(solution (S, K)); // output: 2
+
+// S = K + 1
+S = 'o iAmEight';
+K = 9;
+console.log(solution (S, K)); // output: 2
+
+/*
+// test input
+
+// K is not a number
+S = 'iAmEight iAmEight iAmEight';
+K = '9';
+console.log(solution (S, K)); // output: error: invalid input; undefined
+
+// K is not int
+S = 'iAmEight iAmEight iAmEight';
+K = 9.5;
+console.log(solution (S, K)); // output: error: invalid input; undefined
+
+// K < 1
+S = 'iAmEight iAmEight iAmEight';
+K = -9;
+console.log(solution (S, K)); // output: error: invalid input; undefined
+
+// K > 500
+S = 'iAmEight iAmEight iAmEight';
+K = 501;
+console.log(solution (S, K)); // output: error: invalid input; undefined
+
+// S is not a string
+S = 99;
+K = 9;
+console.log(solution (S, K)); // output: error: invalid input; undefined
+
+// S has wrong characters
+S = '#@)₴?$0!';
+K = 9;
+console.log(solution (S, K)); // output: error: invalid input; undefined
+
+// S has 2 spaces inside
+S = 'iAmEight  iAmEight iAmEight';
+K = 9;
+console.log(solution (S, K)); // output: error: invalid input; undefined
+
+// S is an empty string
+S = '';
+K = 9;
+console.log(solution (S, K)); // output: error: invalid input; undefined
+
+// S has more than 500 characters
+S = 'caecilii caeco caelo calere campum canes captet capti captiosa careat carere careret caret caritatem carum causa causae causam causas cedentem celeritas censes censet centurionum cepisse ceramico cernantur cernimus certa certae certamen certe certissimam ceteris cetero ceterorum ceteros choro chorusque chremes chrysippe chrysippi chrysippo cibo cillum circumcisaque cives civibus civitas civitatis civium clamat clariora claris clarorum class claudicare clita coercendi coerceri cogitarent cogitemus'
+K = 99;
+console.log(solution (S, K)); // output: error: invalid input; undefined
+
+// S has spaces at start
+S = ' iAmEight iAmEight iAmEight';
+K = 9;
+console.log(solution (S, K)); // output: error: invalid input; undefined
+
+// S has spaces at the end
+S = 'iAmEight iAmEight iAmEight ';
+K = 9;
+console.log(solution (S, K)); // output: error: invalid input; undefined
+*/
