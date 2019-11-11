dfda29ec4e96267bccfb48c6c3b4a5a33aab8173
olena-olena <vykaliuk@gmail.com>
2019-11-11 18:52:11 +0200

    + input validation

--- stringToSMS.js
+++ stringToSMS.js
@@ -1,8 +1,18 @@
-function solution (S,K) {
+function solution (S, K) {
+
+	// test if input is valid for function
+	if (typeof(K)!=='number' || Math.floor(K)!==K || K<1 || K>500 ||
+		typeof(S)!=='string' || S==='' || S.length>500 || S.includes('  ') ||
+		S[0]===' ' ||S[S.length-1]===' ' || /[^a-z ]/i.test(S) ) {
+		console.log('error: invalid input');
+		return;
+	}
+	
 	
 	let N = 0;
+	// console.log(S) // to watch what's going on
 	
-	// test if splitting by K is doable for every word
+	// test if splitting by K is possible for every word
 	let testS = S.split(' ');
 	for ( let j = 0; j < testS.length; j++) {
 		if ( testS[j].length > K) {
@@ -10,11 +20,12 @@ function solution (S,K) {
 		}
 	}
 
-        // counting sms
+    // counting sms
 	while (S.length > K) {
 		for ( let i = K; i >= 0; i--) {
 			if (S[i] === ' ') {
 				S = S.slice(i+1);
+				// console.log(S) // to watch what's going on
 				N++;
 				break;
 			}
@@ -30,8 +41,21 @@ function solution (S,K) {
 }
 
 
-// test
-let S = 'sms messages are really short';
-let K = 12;
 
-console.log(solution (S, K));
+// test area
+
+// ok
+let S = 'SMS messases are really short';
+let K = 12;
+console.log(solution (S, K), '\n'); // output: 3
+
+// K is too small
+S = 'SMS messases are really short';
+K = 3;
+console.log(solution (S, K), '\n'); // output: -1
+
+// wrong characters in string
+S = '#@)₴?$0';
+K = 10;
+console.log(solution (S, K), '\n'); // output: error: invalid input; undefined 
+
